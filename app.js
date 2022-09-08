c716067d43b7fa32f5a622b30003759d14bf4157
johnny L <john.lingafelter@snhu.edu>
2022-09-08 15:10:48 -0700

    Express website, basic MVC routing

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
+const { handlebars } = require('hbs');
+
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server','views'));
+
+// register hbs partials
+handlebars.registerPartial('header', path.join(__dirname, 'app_server', 'views/partials/header.hbs'));
+handlebars.registerPartial('footer', path.join(__dirname, 'app_server', 'views/partials/footer.hbs'));
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -21,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
